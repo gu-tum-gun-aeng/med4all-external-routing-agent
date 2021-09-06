a876d884abf4a6f0d09afd218ea84b2d5f1a0ce7
veerapat.b <veerapat.b@ava.fund>
2021-09-06 19:03:58 +0700

    added colink config

--- src/config.ts
+++ src/config.ts
@@ -12,11 +12,11 @@ const config: {
 } = {
   appName: jsonConfig.get("app.name"),
   logLevel: jsonConfig.get("log.level"),
-  kafkaBrokerList: (process.env.KAFKA_BROKER_LIST || "").split(","),
+  kafkaBrokerList: (process.env.KAFKA_BROKER_LIST || jsonConfig.get("kafka.brokers")!).split(","),
   kafkaGroupId: process.env.KAFKA_GROUP_ID || "",
   kafkaConnectionTimeout: Number(process.env.KAFKA_CONNECTION_TIMEOUT) || 1000,
-  colinkApiUrl: process.env.COLINK_API_URL || "",
-  colinkApiKey: process.env.COLINK_API_KEY || "",
+  colinkApiUrl: process.env.COLINK_API_URL || jsonConfig.get("colink.url"),
+  colinkApiKey: process.env.COLINK_API_KEY || jsonConfig.get("colink.token"),
 }
 
 export default config
--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,5 @@ const run = async () => {
   await colinkProcessor.consumePatientWithRiskScore()
   logger.info("Terminating Application")
 }
+
 run().catch(logger.error)
