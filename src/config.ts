276d664795460f823fee1fbaf2eac29cc272011d
veerapat.b <veerapat.b@ava.fund>
2021-09-06 19:18:29 +0700

    added kafka broker json config

--- src/config.ts
+++ src/config.ts
@@ -12,7 +12,7 @@ const config: {
 } = {
   appName: jsonConfig.get("app.name"),
   logLevel: jsonConfig.get("log.level"),
-  kafkaBrokerList: (process.env.KAFKA_BROKER_LIST || jsonConfig.get("kafka.brokers")!).split(","),
+  kafkaBrokerList: (process.env.KAFKA_BROKER_LIST || jsonConfig.get("kafka.brokers"))!.split(","),
   kafkaGroupId: process.env.KAFKA_GROUP_ID || "",
   kafkaConnectionTimeout: Number(process.env.KAFKA_CONNECTION_TIMEOUT) || 1000,
   colinkApiUrl: process.env.COLINK_API_URL || jsonConfig.get("colink.url"),
